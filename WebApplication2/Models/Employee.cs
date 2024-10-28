ea72f53b690c1cf63193f5c8aa97ef5ac5825599
abdalazimomar <abedb@DESKTOP-77LA0HR>
2024-10-28 20:46:34 +0300

    Add project files.

---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Position { get; set; }
+
+    public string? Department { get; set; }
+
+    public decimal? Salary { get; set; }
+
+    public DateOnly? HireDate { get; set; }
+}
